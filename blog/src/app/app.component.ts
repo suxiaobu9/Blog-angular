338bf7a632f14f9dac7ddea490e6f74ef7870288
Ari <ari@miniasp.com>
2021-07-09 00:19:52 +0800

    side bar

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { faRobot, faTorah, faPray } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'blog';
+  ngOnInit() {}
+
+  closeSideBar: boolean = false;
+  sideBarItem = [
+    {
+      name: '技術',
+      href: '#',
+      icon: faRobot,
+      marginHori: 10,
+    },
+    {
+      name: '食譜',
+      href: '#',
+      icon: faTorah,
+      marginHori: 10,
+    },
+    {
+      name: '面試',
+      href: '#',
+      icon: faPray,
+      marginHori: 15,
+    },
+  ];
+
+  toggleSideBar() {
+    this.closeSideBar = !this.closeSideBar;
+  }
 }
